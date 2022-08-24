56a7162cdc8fd6b474cd22d4678ad3bef501f906
Soufian-Najafi <77900265+Soufian-Najafi@users.noreply.github.com>
2022-08-24 10:32:26 +0200

    refactoring: component send request

--- vagabundo-frontend/src/app/emission/company-emission/company-emission.component.ts
+++ vagabundo-frontend/src/app/emission/company-emission/company-emission.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { StatsSelectionService } from '@statsselectionsevice/statsselection.service';
 import { GranularityViewLevel } from '@models/granularityViewLevel';
+import {Observable} from 'rxjs';
+import {DateModel} from '@store/models/date';
+import {select, Store} from '@ngrx/store';
+import {getAllDates} from '@store/selectors/date.selectors';
+import {State} from '@app/store/reducers';
+import {GetTeamStats} from '@store/actions/team-stat.actions';
 
 @Component({
   selector: 'app-company-emission',
@@ -9,11 +15,28 @@ import { GranularityViewLevel } from '@models/granularityViewLevel';
 })
 export class CompanyEmissionComponent implements OnInit {
 
+  private dates$: Observable<DateModel> = this.store.pipe(select(getAllDates));
+
+  dates: DateModel = {
+    fromDate: null,
+    toDate: null
+  };
+
   constructor(
+    private store: Store<State>,
     private statsSelection: StatsSelectionService
   ) { }
 
   ngOnInit() {
     this.statsSelection.selectStats(GranularityViewLevel.Company);
+
+    this.dates$.subscribe((dates: DateModel) => {
+      this.dates = dates;
+
+      this.store.dispatch(new GetTeamStats({
+        fromDate: this.dates.fromDate,
+        toDate: this.dates.toDate
+      }));
+    });
   }
 }
